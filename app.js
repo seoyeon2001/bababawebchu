8facf995d39b349a97336014192f3dfc507158e2
seoyeon2001 <syeon1250@naver.com>
2023-11-23 00:37:37 +0900

    회원가까지 라우터 경로 설정

--- app.js
+++ app.js
@@ -36,8 +36,13 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
+  res.status(err.status || 3000);
   res.send("something wrong!");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('500번 오류!');
 });
 
 module.exports = app;
