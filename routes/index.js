6b29aca114aba42c2f51eb89bdbc19278b52d973
Heewon318 <mhw1514@kw.ac.kr>
2023-11-29 22:59:10 +0900

    불필요한 공백 제거

--- routes/index.js
+++ routes/index.js
@@ -17,18 +17,14 @@ router.get("/", function (req, res, next) {
 
 router.get("/navbar", function (req, res, next) {
   fs.readFile("./views/navbar.html", (err, data) => {
-      if (err) {
+    if (err) {
       res.send("error");
-      } else {
+    } else {
       res.writeHead(200, { "Content-Type": "text/html" });
       res.write(data);
       res.end();
-      }
+    }
   });
 });
-
-
-
-
 
 module.exports = router;
