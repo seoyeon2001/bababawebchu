139f5ba1ee8e95c115e9266c8f2869881a7914f7
g0yujin <dbwbsqhd016@gmail.com>
2023-12-01 23:13:02 +0900

    mongoose/schemas/match.js와 board.js 추가

---
+++ mongoose/schemas/board.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const boardSchema = new Schema({
+    writer : { // 유저 아이디
+        type : Schema.Types.ObjectId,
+        required : true,
+        ref: 'User'
+    },
+    id: { //게시글 번호
+        type : String,
+        required : true
+    },
+    title: { // 제목
+        type : String,
+        required : true
+    },
+
+    content : { // 내용
+        type : String,
+        required : true
+    },
+    createdAt:{ // 글을 작성한 날짜 
+        type : Date,
+        default : Date.now,
+        required : true
+    },
+    removedAt:{ // 글을 삭제 날짜 
+        type : Date,
+        default : Date.now
+    },
+
+    editedAt:{ // 글을 수정한 날짜 
+        type : Date,
+        default : Date.now
+    },
+    category:{
+        type: String,
+        enum: ['popular', 'daily','equipment','tip','market','promotion']
+        
+    }
+
+})
+ 
+ 
+ 
+ const Board = mongoose.model('Board', boardSchema);
+ 
+ module.exports = { Board }
---
+++ mongoose/schemas/match.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const matchSchema = new Schema({
+    writer : { // 유저 아이디
+        type : Schema.Types.ObjectId,
+        required : true,
+        ref: 'User'
+    },
+    id: { //게시글 번호
+        type : String,
+        required : true
+    },
+    title: { // 제목
+        type : String,
+        required : true
+    },
+    location:{ //장소
+        type : String,
+        required : true
+    },
+    matchDate:{ //매치 날짜
+        type : Date,
+        required : true
+    },
+
+    price:{// 금액
+        type : Number,
+        required : true
+    },
+    people : { // 모집 인원 
+        type : Number,
+        required : true
+    },
+    gender:{ //성별
+        type: String,
+        enum: ['male', 'female','irrelevant'],
+        required : true
+    },
+    sport:{ // 운동 종목
+        type : String,
+        required : true
+    },
+
+    content : { // 내용
+        type : String,
+        required : true
+    },
+    createdAt:{ // 글을 작성한 날짜 
+        type : Date,
+        default : Date.now,
+        required : true
+    },
+    removedAt:{ // 글을 삭제 날짜 
+        type : Date,
+        default : Date.now
+    },
+
+    editedAt:{ // 글을 수정한 날짜 
+        type : Date,
+        default : Date.now
+    }
+
+ })
+ 
+ 
+ 
+ const Board = mongoose.model('Match', matchSchema);
+ 
+ module.exports = { Match }
